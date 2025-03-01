5a15cfc202f1acfbca0c50fc268bdc6b8c883cae
achtaitaipai <charles.cailleteau@gmail.com>
2025-03-01 22:01:53 +0100

    simplify upload files

--- vite/src/admin.ts
+++ vite/src/admin.ts
@@ -1,7 +1,6 @@
 import "./admin/style.css";
 import { CopyBtn } from "./admin/webComponents/CopyBtn";
 import { TextEditor } from "./admin/webComponents/TextEditor";
-import "@grafikart/drop-files-element";
 
 customElements.define("text-editor", TextEditor);
 
